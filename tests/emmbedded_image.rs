5e9e8f73394b7dd927a15b591c7209ccbec0158d
Yuriy Larin <blandger@gmail.com>
2025-12-17 16:16:03 +0200

    added embedded images book

---
+++ tests/emmbedded_image.rs
@@ -0,0 +1,35 @@
+use serial_test::serial;
+use std::path::Path;
+use tracing::debug;
+mod common;
+use crate::common::epub::{generate_epub, output_epub_is_valid};
+
+#[test]
+#[serial]
+fn test_embedded_image_tag() {
+    debug!("test_embedded_image_tag...");
+    let doc = generate_epub("embedded_image");
+    println!("{:?}", doc);
+    assert!(doc.is_ok());
+    let mut doc = doc.unwrap();
+    debug!("doc current path = {:?}", doc.1);
+
+    let path = if cfg!(target_os = "linux") {
+        Path::new("OEBPS").join("chapter_1.html") // linux
+    } else {
+        Path::new("OEBPS/chapter_1.html").to_path_buf() // windows with 'forward slash' /
+    };
+    let file = doc.0.get_resource_str_by_path(path);
+    let content = file.unwrap();
+    debug!("content =\n{:?}", content);
+    // assert!(content.contains("<img src=\"b270cb6837d41f98.png\" alt=\"Image\" />"));
+    // assert!(content.contains("<img src=\"4dbdb25800b6fa1b.jpg\" alt=\"Image\" />"));
+}
+
+// #[ignore = "CI/CD only"]
+#[test]
+#[serial]
+fn test_output_embedded_image_is_valid() {
+    // output_epub_is_valid("remote_image_fetch");
+    common::epub::output_epub_is_valid_preserve_temp_folder("embedded_image");
+}
